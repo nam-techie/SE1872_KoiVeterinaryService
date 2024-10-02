5fea79ec998a1d5a35bf7ea9ac2bc5b23ccb292c
khiemabc <khiemngse182188@fpt.edu.vn>
2024-10-02 14:01:23 +0700

    Add security config

--- src/main/java/com/namtechie/org/controller/AuthenticationController.java
+++ src/main/java/com/namtechie/org/controller/AuthenticationController.java
@@ -8,10 +8,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api")
 @RestController
@@ -20,8 +17,12 @@ public class AuthenticationController {
 
     @Autowired
     AuthenticationService authenticationService;
+
+
+    @CrossOrigin(origins = "http://localhost:5741")
     @PostMapping(value = "/register", produces = "application/json")
     public ResponseEntity register(@Valid @RequestBody RegisterRequest registerRequest) {
+        System.out.println("Vcl");
         AccountResponse newAccount = authenticationService.register(registerRequest);
         return ResponseEntity.ok(newAccount);
     }
