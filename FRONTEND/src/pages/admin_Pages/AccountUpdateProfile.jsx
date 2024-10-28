f17535305cd1de65c43335567c027e8905bad214
Winnn2004 <annhnse180074@fpt.edu.vn>
2024-10-28 22:53:34 +0700

    fix path

--- FRONTEND/src/pages/admin_Pages/AccountUpdateProfile.jsx
+++ FRONTEND/src/pages/admin_Pages/AccountUpdateProfile.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 import { useAccountInfo } from './hooks/useAccountInfo';
-import '../admin_pages/styles/AccountUpdateProfile.css';
+import './styles/AccountUpdateProfile.css';
 
 // eslint-disable-next-line react/prop-types
 const AccountUpdateProfile = ({ account, onClose, onUpdate }) => {
