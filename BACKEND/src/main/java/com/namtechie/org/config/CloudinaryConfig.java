d3897fae5cdcce3e4e07910a9fbab10f0e93bb5a
Winnn2004 <annhnse180074@fpt.edu.vn>
2024-10-28 20:59:31 +0700

    hide key

--- BACKEND/src/main/java/com/namtechie/org/config/CloudinaryConfig.java
+++ BACKEND/src/main/java/com/namtechie/org/config/CloudinaryConfig.java
@@ -1,6 +1,8 @@
 package com.namtechie.org.config;
 
 import com.cloudinary.Cloudinary;
+import com.google.api.client.util.Value;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,12 +11,26 @@ import java.util.Map;
 
 @Configuration
 public class CloudinaryConfig {
+    @Value("${cloudinary.cloud-name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api-key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api-secret}")
+    private String apiSecret;
+
+    private Cloudinary cloudinary;
+
+
     @Bean
     public Cloudinary cloudinary() {
+        Dotenv dotenv = Dotenv.load();
+
         final Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", "dxxrlgi21");
-        config.put("api_key", "816468662669557");
-        config.put("api_secret", "a_z6VzYFfc61PXar9c3RuFJd9Vw");
+        config.put("cloud_name", dotenv.get("CLOUDINARY_CLOUD_NAME"));
+        config.put("api_key", dotenv.get("CLOUDINARY_API_KEY"));
+        config.put("api_secret", dotenv.get("CLOUDINARY_API_SECRET"));
         return new Cloudinary(config);
     }
 }
