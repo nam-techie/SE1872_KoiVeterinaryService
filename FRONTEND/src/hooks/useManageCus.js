19c028492a06e868a0b400a0d7e9d9779453894f
nam-techie <namdpse180259@fpt.edu.vn>
2024-10-31 08:40:12 +0700

    Nam fix import

--- FRONTEND/src/hooks/useManageCus.js
+++ FRONTEND/src/hooks/useManageCus.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import {axiosInstance} from "../service/apiRequest.js";
+import { axiosInstance } from '../service/apiRequest';
 
 const useManageCus = () => {
     const getAppointments = async () => {
@@ -7,22 +7,6 @@ const useManageCus = () => {
             const response = await axiosInstance.get(
                 `/customer/listAppointmentUser`
             );
-    const [appointments, setAppointments] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        const fetchAppointments = async () => {
-            try {
-                const username = localStorage.getItem('username');
-                console.log(username);
-                if (!username) {
-                    throw new Error('Không tìm thấy thông tin người dùng');
-                }
-
-                const response = await axiosInstance.get(
-                    `/customer/listAppointmentByUsername`
-                );
 
             console.log('Response from API:', response.data);
 
