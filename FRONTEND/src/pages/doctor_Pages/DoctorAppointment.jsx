ddfdd9a93d75e95bb4c22a9af4e6282fcd3ead1e
NhatAn <annhnse180074@fpt.edu.vn>
2024-11-09 17:32:38 +0700

    fix show time doctor

--- FRONTEND/src/pages/doctor_Pages/DoctorAppointment.jsx
+++ FRONTEND/src/pages/doctor_Pages/DoctorAppointment.jsx
@@ -631,7 +631,28 @@ function DoctorAppointment() {
                     loading={loading}
                     columns={[
                         { title: 'ID lịch hẹn', dataIndex: 'id', key: 'id' },
-                        { title: 'Thời gian', dataIndex: 'time', key: 'time' },
+                        { 
+                            title: 'Thời gian', 
+                            dataIndex: 'time', 
+                            key: 'time', 
+                            render: (time) => {
+                                // Kết hợp với ngày hiện tại để tạo đối tượng Date hợp lệ
+                                const today = new Date();
+                                const dateString = `${today.toISOString().split('T')[0]}T${time}`;
+                                const date = new Date(dateString);
+                                
+                                return (
+                                    <span>
+                                        {new Intl.DateTimeFormat('vi-VN', { 
+                                            timeZone: 'Asia/Ho_Chi_Minh', 
+                                            hour: 'numeric', 
+                                            minute: 'numeric', 
+                                            second: 'numeric' 
+                                        }).format(date)}
+                                    </span>
+                                );
+                            } 
+                        },
                         { title: 'Ngày', dataIndex: 'date', key: 'date' },
                         { title: 'Dịch vụ', dataIndex: 'service', key: 'service' },
                         { title: 'Trạng thái', 
